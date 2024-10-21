758c40198f9f7ae0f0741f85f81cdfe94ded6ef1
kuldeepSonkusare <sonkusarekuldeep@gmail.com>
2024-10-21 14:40:40 +0530

    test

--- stringCase1.js
+++ stringCase1.js
@@ -2,6 +2,25 @@
 
 // Example: toCase('Mthatha') should return 'mthatha-MTHATHA'.
 
-function toCase(x){
+function toCase(x) {
     return x.toLowerCase() + '-' + x.toUpperCase();
-    }
+}
+
+// Write a function secondIndexOf, taking two strings and determining the second occurrence of the second string in the first string. If the search string does not occur twice, -1 should be returned.
+
+// Example: secondIndexOf('White Rabbit', 'it') should return 10.
+
+function secondIndexOf(s1, s2) {
+    let firstIndex = s1.indexOf(s2);
+    return s1.indexOf(s2, firstIndex + 1);
+}
+
+// Write a function secondIndexOf, taking two strings and determining the second occurrence of the second string in the first string. If the search string does not occur twice, -1 should be returned.
+
+// Example: secondIndexOf('White Rabbit', 'it') should return 10.
+function secondIndexOf(x, y) {
+    let first = x.indexOf(y);
+    return x.indexOf(y, first + 1);
+}
+
+
